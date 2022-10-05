6319ecfa509a5c90b1cf6d16c2dd851a64126217
Arechi <aglimbenli@gmail.com>
2022-10-05 20:53:37 +0200

    Refactor textures

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,5 +1,5 @@
 import { usePlane } from "@react-three/cannon";
-import { NearestFilter, RepeatWrapping, Vector2Tuple, Vector3Tuple } from "three";
+import { Vector2Tuple, Vector3Tuple } from "three";
 import { groundTexture } from "../textures";
 
 const dimensions: Vector2Tuple = [100, 100];
@@ -8,11 +8,6 @@ const rotation: Vector3Tuple = [-Math.PI / 2, 0, 0];
 
 const Ground = () => {
   const [ref] = usePlane(() => ({ position, rotation }));
-
-  groundTexture.magFilter = NearestFilter;
-  groundTexture.wrapS = RepeatWrapping;
-  groundTexture.wrapT = RepeatWrapping;
-  groundTexture.repeat.set(100, 100);
 
   return (
     <mesh ref={ref as any}>
--- src/textures.ts
+++ src/textures.ts
@@ -1,4 +1,4 @@
-import { TextureLoader } from "three";
+import { NearestFilter, RepeatWrapping, Texture, TextureLoader } from "three";
 
 export const dirtTexture = new TextureLoader().load("images/dirt.jpg");
 export const glassTexture = new TextureLoader().load("images/glass.png");
@@ -6,3 +6,18 @@ export const grassTexture = new TextureLoader().load("images/grass.jpg");
 export const logTexture = new TextureLoader().load("images/log.jpg");
 export const woodTexture = new TextureLoader().load("images/wood.png");
 export const groundTexture = grassTexture;
+
+const textures: { [key: string]: Texture } = {
+  dirt: dirtTexture,
+  glass: glassTexture,
+  grass: grassTexture,
+  log: logTexture,
+  wood: woodTexture
+};
+
+Object.values(textures).forEach(texture => (texture.magFilter = NearestFilter));
+groundTexture.wrapS = RepeatWrapping;
+groundTexture.wrapT = RepeatWrapping;
+groundTexture.repeat.set(100, 100);
+
+export default textures;
