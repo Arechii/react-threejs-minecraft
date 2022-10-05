1084e2659e7123afb66cfbd382e03fafb6a79161
Arechi <aglimbenli@gmail.com>
2022-10-05 18:37:27 +0200

    Add ground

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { Physics } from "@react-three/cannon";
 import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import Ground from "./components/Ground";
 
-const sunPosition = [100, 100, 20];
+const sunPosition = [100, 100, 20] as const;
+const lightIntensity = 0.5 as const;
 
 const App = () => {
   return (
     <>
       <Canvas>
         <Sky sunPosition={sunPosition} />
+        <ambientLight intensity={lightIntensity} />
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>
     </>
   );
---
+++ src/components/Ground.tsx
@@ -0,0 +1,25 @@
+import { usePlane } from "@react-three/cannon";
+import { NearestFilter, RepeatWrapping, Vector2Tuple, Vector3Tuple } from "three";
+import { groundTexture } from "../textures";
+
+const dimensions: Vector2Tuple = [100, 100];
+const position: Vector3Tuple = [0, 0, 0];
+const rotation: Vector3Tuple = [-Math.PI / 4, 0, 0];
+
+const Ground = () => {
+  const [ref] = usePlane(() => ({ position, rotation })) as any;
+
+  groundTexture.magFilter = NearestFilter;
+  groundTexture.wrapS = RepeatWrapping;
+  groundTexture.wrapT = RepeatWrapping;
+  groundTexture.repeat.set(100, 100);
+
+  return (
+    <mesh ref={ref}>
+      <planeGeometry attach="geometry" args={dimensions} />
+      <meshStandardMaterial attach="material" map={groundTexture} />
+    </mesh>
+  );
+};
+
+export default Ground;
