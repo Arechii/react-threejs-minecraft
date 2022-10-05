78eca4870bdc9ad97acb92296e26d5e079a7d02e
Arechi <aglimbenli@gmail.com>
2022-10-05 19:45:08 +0200

    Add FPV component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Player from "./components/Player";
 import Ground from "./components/Ground";
+import FPV from "./components/FPV";
 
 const sunPosition = [100, 100, 20] as const;
 const lightIntensity = 0.5 as const;
@@ -13,11 +14,13 @@ const App = () => {
       <Canvas>
         <Sky sunPosition={sunPosition} />
         <ambientLight intensity={lightIntensity} />
+        <FPV />
         <Physics>
           <Player />
           <Ground />
         </Physics>
       </Canvas>
+      <div className="cursor absolute centered">+</div>
     </>
   );
 };
