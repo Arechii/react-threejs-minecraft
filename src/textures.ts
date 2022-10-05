e4baa69311a644739f44e6e2eb3f9b695698d134
Arechi <aglimbenli@gmail.com>
2022-10-05 18:20:46 +0200

    Add textures

---
+++ src/textures.ts
@@ -0,0 +1,8 @@
+import { TextureLoader } from "three";
+
+export const dirtTexture = new TextureLoader().load("images/dirt.jpg");
+export const glassTexture = new TextureLoader().load("images/glass.png");
+export const grassTexture = new TextureLoader().load("images/grass.jpg");
+export const logTexture = new TextureLoader().load("images/log.jpg");
+export const woodTexture = new TextureLoader().load("images/wood.png");
+export const groundTexture = grassTexture;
